adc75bd963a0f6e2c5ae0eccffe9d96efe6c17e2
Dan Lambert <dan.lambert4@gmail.com>
2021-10-20 14:03:08 -0500

    solved problem 6

--- src/problem6.py
+++ src/problem6.py
@@ -5,11 +5,37 @@ def main():
     # Record start time
     startTime = time()
     
-    
+    # Number to sum against
+    number = 100
+
+    # Calculate answer
+    answer = squareOfSums(number) - sumOfSquares(number)
+    print(f'The answer is {answer}')
 
     # Record end time, print total run time
     endTime = time()
     print(f'Runtime of the program is {endTime - startTime}')
+
+def sumOfSquares(num):
+    # Declare initial variable
+    answer = 0
+
+    # Loop all integars up to num and square then sum
+    for i in range(1, num + 1):
+        answer += pow(i, 2)
+    
+    return answer
+
+def squareOfSums(num):
+    # Declare initial variable
+    answer = 0
+
+    # Loop all integars up to num and sum then square
+    for i in range(1, num + 1):
+        answer += i
+    answer = pow(answer, 2)
+
+    return answer
 
 if __name__ == '__main__':
     main()
